779cfd6b6ad3991599b37e1ee0b9e50d2590ee30
RIteshSolidity <34639678+RIteshSolidity@users.noreply.github.com>
2020-05-22 13:46:32 +0530

    Create ServiceBusTopicSender.cs

---
+++ ServiceBusTopicSender.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace SenderTopic
+{
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string OrderDescription { get; set; }
+    }
+
+    class Program
+    {
+
+        static string connectionString = "Endpoint=sb://samplesbthursday.servicebus.windows.net/;SharedAccessKeyName=sendonly;SharedAccessKey=A9yj1vsgUkRBQXPgxL5GuXjx52z542CrWbz90qg6+1s=";
+        static void Main(string[] args)
+        {
+            TopicClient client = new TopicClient(connectionString, "testtopic");
+
+            Message message = new Message();
+
+            Order o = new Order
+            {
+                OrderDescription = "samsung phones",
+                OrderId = "A001"
+            };
+
+
+            string msg = JsonConvert.SerializeObject(o);
+
+            message.Body = System.Text.Encoding.ASCII.GetBytes(msg);
+
+            message.ContentType = "application/json";
+
+            client.SendAsync(message).GetAwaiter().GetResult();
+        }
+    }
+}
